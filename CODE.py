6ccdceb768e9ed75fdbc75f0165b6a8b31b75f80
R3YZE1 <92102076+R3YZE1@users.noreply.github.com>
2024-12-22 20:39:25 +0530

    added developer info to CODE.py

--- CODE.py
+++ CODE.py
@@ -1,4 +1,5 @@
 ''' AUTHORS
-RISHI - https://github.com/R3YZE1
-RAVISHANKARA - https://github.com/Rav0110
+TEAM HANGMAN (CSE - G)
+R3YZE1 - RISHI - https://github.com/R3YZE1
+Rav0110 - RAVISHANKARA - https://github.com/Rav0110
 RHEA MADHUKUMAR '''
